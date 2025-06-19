c93569319df47cd27fb0442967734601f2123910
game347 <thanakornguide2547@gmail.com>
2025-06-19 20:55:46 +0700

    3

--- frontend1/src/pages/Balance.tsx
+++ frontend1/src/pages/Balance.tsx
@@ -6,9 +6,10 @@ const Balance = () => {
     <div className="min-h-screen w-screen bg-white flex items-center justify-center">
       <div className="px-6 pt-12">
         <div className="bg-white rounded-2xl shadow-lg p-6 border border-gray-100">
-          <div className="text-center">
-            <h1 className="text-2xl font-bold text-gray-800">Balance</h1>
+          <div className="text-left">
+            <h4 className="text-2xl font-bold text-gray-800">Balance</h4>
             <p className="text-gray-600 mt-2">Your account balance details</p>
+            <p className="text-gray-1000 font-bold mt-2">xxx,xxx,xxx</p>
           </div>
         </div>
       </div>
